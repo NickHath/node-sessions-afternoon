6738ca303f2ed9a63574c0f875e1487abfc34227
snguyen618 <snguyen618@gmail.com>
2017-10-17 15:02:33 -0600

    step 3 and 4 completed

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
       cors = require('cors'),
-      session = require('express-session');
+      session = require('express-session'),
+      checkForSessions = require('./middlewares/checkForSession');
 
 const app = express();
 app.use(cors());
@@ -10,7 +11,9 @@ app.use(session({
   secret: 'fjhadsifhasilffihasjdfasfasjf',
   resave: false,
   saveUninitialized: true
-}))
+}));
+
+app.use(checkForSessions);
 
 const PORT = 4200;
 app.listen(PORT, () => console.log('Listening on port: ' + PORT));
